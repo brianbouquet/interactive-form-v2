58cd1833367f5d8e019290611b62329bdafa2b8a
brianbouquet <59177725+brianbouquet@users.noreply.github.com>
2021-01-19 12:02:28 -0700

    set the focus to first input

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
-const focusInput = document.getElementById('name');
-console.log(focusInput);
+//select form elements
+var inputName = document.getElementById('name');
+var inputEmail = document.getElementById('mail');
+var selectTitle = document.getElementById('title');
+var selectSize = document.getElementById('size');
+
+//set the focus to first input
+window.onload = function getFocus() {
+    inputName.focus();
+}
