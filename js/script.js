54e9dcd1571ec50171c07b2294b17b4cf286cad3
brianbouquet <59177725+brianbouquet@users.noreply.github.com>
2021-01-19 13:28:49 -0700

    created "other" job title input logic
    Created "other" job title input in HTML file
    Hid the "other" job title input initially
    Added event listener and show/hide "other" job title input logic to Job Title dropdown

--- js/script.js
+++ js/script.js
@@ -3,8 +3,31 @@ var inputName = document.getElementById('name');
 var inputEmail = document.getElementById('mail');
 var selectTitle = document.getElementById('title');
 var selectSize = document.getElementById('size');
+var otherTitle = document.getElementById('other-title');
 
 //set the focus to first input
 window.onload = function getFocus() {
     inputName.focus();
 }
+
+//function to create form element
+function createFormElement(elementName, type, id, name, placeholder) {
+    var element = document.createElement(elementName);
+    element.setAttribute("type", type);
+    element.id = id;
+    element.setAttribute("name", name);
+    element.setAttribute("placeholder", placeholder);
+    return element;
+}
+
+//initially hide "other" job title input
+otherTitle.hidden = true;
+
+//check job title value for other and show/hide input
+selectTitle.addEventListener("change", (event) => {
+    if (event.target.value == "other") {
+        otherTitle.hidden = false;
+    } else {
+        otherTitle.hidden = true;
+    }
+});
