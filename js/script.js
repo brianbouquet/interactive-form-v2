cee79b99c2e95627fcb536660e0e5e150b3d459a
brianbouquet <59177725+brianbouquet@users.noreply.github.com>
2021-01-19 22:29:25 -0700

    added selection dependency for t-shirt theme and color options

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ var inputName = document.getElementById('name');
 var inputEmail = document.getElementById('mail');
 var selectTitle = document.getElementById('title');
 var selectSize = document.getElementById('size');
+var selectColor = document.getElementById('color');
+var selectDesign = document.getElementById('design');
 var otherTitle = document.getElementById('other-title');
 
 //set the focus to first input
@@ -31,3 +33,70 @@ selectTitle.addEventListener("change", (event) => {
         otherTitle.hidden = true;
     }
 });
+
+//set "Color" field to "Please select a T-shirt theme" by default
+var colorDefault = document.createElement('option');
+colorDefault.textContent = "Please select a T-shirt theme";
+selectColor.prepend(colorDefault);
+var selectColorOptions = document.querySelectorAll('#shirt-colors option');
+selectColorOptions[0].selected = true;
+
+//hide "Color" options
+for (var i = 0; i < selectColorOptions.length; i++) {
+    var optionValue = selectColorOptions[i].value;
+    if (optionValue !== "Please select a T-shirt theme") {
+        selectColorOptions[i].hidden = true;
+    }
+}
+
+//add event listener to show colors when design theme selected
+selectDesign.addEventListener("change", (event) => {
+    //show "JS Puns" theme colors
+    var designValue = event.target.value;
+    if (designValue == "js puns") {
+        selectColorOptions[1].selected = true;
+        for (var i = 0; i < selectColorOptions.length; i++) {
+            var optionValue = selectColorOptions[i].value;
+            console.log(optionValue);
+            if (optionValue == "cornflowerblue" || optionValue == "darkslategrey" || optionValue == "gold") {
+                selectColorOptions[i].hidden = false;
+            } else {
+                selectColorOptions[i].hidden = true;
+            }
+        }
+    } else if (designValue == "heart js") {
+        selectColorOptions[4].selected = true;
+        for (var i = 0; i < selectColorOptions.length; i++) {
+            var optionValue = selectColorOptions[i].value;
+            console.log(optionValue);
+            if (optionValue == "tomato" || optionValue == "steelblue" || optionValue == "dimgrey") {
+                selectColorOptions[i].hidden = false;
+            } else {
+                selectColorOptions[i].hidden = true;
+            }
+        }
+    } else {
+        selectColorOptions[0].selected = true;
+        for (var i = 0; i < selectColorOptions.length; i++) {
+            var optionValue = selectColorOptions[i].value;
+            if (optionValue !== "Please select a T-shirt theme") {
+                selectColorOptions[i].hidden = true;
+            }
+        }
+    }
+});
+
+// for (var i = 0; i < selectColorOptions.length; i++) {
+//     var optionValue = selectColorOptions[i].value;
+//     console.log(optionValue);
+    // if (optionValue == null) {
+    //     selectColorOptions[i].hidden = true;
+    // } else {
+    //     selectColorOptions[i].hidden = false;
+    // }
+//}
+
+// selectDesign.addEventListener("change", (event) => {
+    
+// });
+
